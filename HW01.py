eb116e4b0f85190f9e7055a9f315c5a7d27bc09f
Yasar2019 <yasar20111926@hotmail.com>
2023-10-30 15:59:59 +0800

    Task #4 Done!

--- HW01.py
+++ HW01.py
@@ -1,29 +1,25 @@
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import SparkSession, Row
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_date
 from operator import add
 import re
 import pandas as pd
-
 import os
-print(os.environ['HADOOP_HOME'])
 
+print(os.environ['HADOOP_HOME'])
 
 def word_count_per_title(title):
     words = re.findall(r'\w+', title)
     return [(word.lower(), 1) for word in words]
-
 
 def word_count_per_date(date_title):
     date, title = date_title
     words = re.findall(r'\w+', title)
     return [(date, word.lower()) for word in words]
 
-
 def clean_text(text):
     if text is None:  # Handle None values
         return []
-
     text = text.lower()  # Convert to lowercase
     text = re.sub(r"'s\b", '', text)  # Remove 's
     text = re.sub(r"'\b", '', text)  # Remove trailing '
@@ -31,18 +27,16 @@ def clean_text(text):
     text = re.sub(r'[^a-z\s]', '', text)  # Remove all non-letter characters
     return text.split()  # Split on whitespace and return the list of words
 
-
 def contains_space(text):
     if not text:  # Check for None or empty strings
         return False
     cleaned_words = clean_text(text)
     return 'space' in cleaned_words
 
-
 if __name__ == "__main__":
     spark = SparkSession.builder \
         .appName("PowerConsumptionStats") \
-        .master("local") \
+        .master("spark://96.9.210.170:7077") \
         .config("spark.hadoop.validateOutputSpecs", "false") \
         .config("spark.hadoop.home.dir", "C:/Users/Asus/Downloads/spark-3.5.0-bin-hadoop3/spark-3.5.0-bin-hadoop3/bin") \
         .getOrCreate()
@@ -50,16 +44,15 @@ if __name__ == "__main__":
         #.config("spark.executor.memory", "4g") \
         #.config("spark.memory.fraction", "0.8") \
         #.config("spark.memory.storageFraction", "0.5") \
-        
 
     sc = spark.sparkContext
 
 
-    #### TASK #1 ##################
+    #TASK #1 
     # Read the data
-    # data = spark.read.csv("spacenews-202309.csv", header=True, inferSchema=True)
     data = spark.read.option("header", "true").option("quote", "\"").option(
         "escape", "\"").option("multiline", "true").csv('spacenews-202309.csv')
+    
     # Extract titles and count words
     titles = data.rdd.map(lambda row: row['title'])
     total_word_counts = titles.flatMap(word_count_per_title).reduceByKey(
@@ -68,19 +61,6 @@ if __name__ == "__main__":
     # Convert total word counts to DataFrame and save to CSV
     total_word_counts_df = total_word_counts.toDF(["word", "count"]).toPandas()
     total_word_counts_df.to_csv("output/total_word_counts.csv", header=True)
-
-    # Extract date and title and count words per date
-    # date_titles = data.rdd.map(lambda row: (row['date'], row['title']))
-    # per_date_word_counts_rdd = date_titles.flatMap(word_count_per_date).map(
-    #    lambda x: (x, 1)).reduceByKey(add).map(lambda x: (x[0][0], x[0][1], x[1]))
-
-    # Convert per date word counts to DataFrame and save to CSV
-    # per_date_word_counts_pd_df = spark.createDataFrame(
-    #    per_date_word_counts_rdd, ["date", "word", "count"]).toPandas()
-    # per_date_word_counts_df.write.partitionBy("date").csv("output/per_date_word_counts", header=True)
-    # for date, group in per_date_word_counts_pd_df.groupby('date'):
-    #   group.drop('date', axis=1).to_csv(
-    #     "output/per_date.csv", index=False)
 
     # Extract date and word from each row
     date_word_rdd = data.rdd.flatMap(
@@ -100,9 +80,7 @@ if __name__ == "__main__":
     word_count_pd.to_csv("output/per_date_word_counts.csv", index=False)
     
     
-    
-    ############### TASK#2 ##################
-    
+    # TASK#2
     # Filter out rows where 'Content' is None and then extract words
     word_rdd = data.rdd.filter(lambda row: row['content'] is not None).flatMap(lambda row: clean_text(row['content']))
 
@@ -118,7 +96,6 @@ if __name__ == "__main__":
 
     # Save the result to a CSV
     total_word_count_pd.to_csv("output/total_content_word_counts.csv", index=False)
-    
     
     # Filter out rows where 'Content' is None, then extract date and word from each row's 'Content'
     date_word_rdd = data.rdd.filter(lambda row: row['content'] is not None).flatMap(
@@ -147,31 +124,44 @@ if __name__ == "__main__":
         "output/per_date_content_word_counts.csv", index=False)
     
     
-    ###################### TASK#4 ###########################
+    #TASK #3 
+    # Percentage of Published Articles in a Day
+    date_count = data.rdd.map(lambda row: (row['date'], 1)).reduceByKey(
+        lambda a, b: a + b).collect()
+    total_articles = sum([count for _, count in date_count])
+    date_percentage = [(date, (count / total_articles) * 100) for date, count in date_count]
     
+    # Percentage of Published Articles by Authors in a Day
+    date_author_count = data.rdd.map(lambda row: (
+        (row['date'], row['author']), 1)).reduceByKey(lambda a, b: a + b).collect()
+    date_author_percentage = [((date, author), (count / dict(date_count)[date]) * 100) for (date, author), count in date_author_count]
+    
+    #Convert to Pandas dataframes
+    date_percentage_df = pd.DataFrame(
+        date_percentage, columns=['Date', 'Percentage'])
+    date_author_percentage_df = pd.DataFrame([(date, author, percentage) for (
+        date, author), percentage in date_author_percentage], columns=['Date', 'Author', 'Percentage'])
+    
+    # Save the output to CSVs files
+    date_percentage_df.to_csv("output/date_percentage.csv", index=False)
+    date_author_percentage_df.to_csv("output/date_author_percentage.csv", index=False)
+    
+    
+    #TASK #4 
     # Map operation: Check if 'Title' and 'Postexcerpt' contain the term "Space"
     def map_filter(row):
         title = row['title'] or ""  # Use an empty string if the title is None
         # Use an empty string if postexcerpt is None
         postexcerpt = row['postexcerpt'] or ""
-
         if "Space" in title and "Space" in postexcerpt:
             return row
         else:
             return None
 
-
     filtered_rdd = data.rdd.map(map_filter).filter(lambda x: x is not None)
-
-    # Convert the filtered RDD to a DataFrame
     filtered_df = spark.createDataFrame(filtered_rdd, data.schema)
-
-    # Convert to Pandas for easier output handling
     filtered_pd = filtered_df.toPandas()
-
-    # Save the result to a CSV
     filtered_pd.to_csv("output/space_in_title_and_postexcerpt.csv", index=False)
-    
     
     # Stop the SparkContext
     sc.stop()
